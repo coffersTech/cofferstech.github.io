7e69ef41dff7ee392ffa25108de07146fce30db5
Ryan <ruifafan@gmail.com>
2026-01-23 23:33:11 +0800

    修改目录问题

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'cofferstech.github.io'; // 你的仓库名
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: isProd ? `/${repoName}` : '',
-    assetPrefix: isProd ? `/${repoName}` : '',
     images: {
         unoptimized: true,
     },
